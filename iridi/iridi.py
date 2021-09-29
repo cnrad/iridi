d3aba92304b8d65bc9c65344b294f92db33eb6c9
cnrad <83192247+cnrad@users.noreply.github.com>
2021-09-29 15:11:32 -0400

    feat: custom preset constructors and print methods

--- iridi/iridi.py
+++ iridi/iridi.py
@@ -90,3 +90,53 @@ class iridi:
                 finalStr += (f"\x1b[38;2;{r};{g};{b}m" + string[index])
 
         return input(finalStr + u"\u001b[0m")
+
+
+
+
+    class preset:
+        def __init__(self, colorArr):
+            self.colorArr = colorArr
+
+        def print(self, string: str, bold: bool = False):
+
+            colorArr = self.colorArr
+
+            length = len(string)
+            colorStopsCount = len(colorArr)
+            sectionLength = math.floor(length/(colorStopsCount - 1))
+
+            if isinstance(colorArr[0], str) and colorArr[0].startswith("#"):
+                for i in range(0, colorStopsCount):
+                    color = colorArr[i].lstrip("#")
+                    rgbColors = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+
+                    colorArr[i] = {
+                        "r": rgbColors[0],
+                        "g": rgbColors[1],
+                        "b": rgbColors[2],
+                    }
+
+            r = int(colorArr[0]["r"])
+            g = int(colorArr[0]["g"])
+            b = int(colorArr[0]["b"])
+
+            if bold:
+                print('\033[1m', end="")
+
+            index = 0
+
+            for i in range(1, colorStopsCount):
+                for j in range(0, sectionLength):
+                    print(f"\x1b[38;2;{r};{g};{b}m" + string[j + index], end='')
+
+                    r += int((colorArr[i]["r"] - colorArr[i - 1]["r"])/sectionLength)
+                    g += int((colorArr[i]["g"] - colorArr[i - 1]["g"])/sectionLength)
+                    b += int((colorArr[i]["b"] - colorArr[i - 1]["b"])/sectionLength)
+
+                index += sectionLength
+
+                if (i + 1 == colorStopsCount) and index < length:
+                    print(f"\x1b[38;2;{r};{g};{b}m" + string[index], end='')
+
+            print(u"\u001b[0m")
--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
-from iridi import *
+import iridi
 
-print(iridi)
 iridi.print("testing for whatever", iridi.presets.instagram)
 res = iridi.input("type any word: ", iridi.presets.aquatic)
 print(res)
+
+gradient = iridi.preset(["#D3959B", "#BFE6BA"])
+gradient.print("Testing with preset constructors.")
